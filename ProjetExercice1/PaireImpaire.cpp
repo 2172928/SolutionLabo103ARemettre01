53eefc784be81d34e91f7e224e69ddc261107e97
Gabriel Gendron <2172928@cstj.qc.ca>
2021-09-02 16:38:27 -0400

    test

--- ProjetExercice1/PaireImpaire.cpp
+++ ProjetExercice1/PaireImpaire.cpp
@@ -1,3 +1,25 @@
-//but
-//auteur
-//date2021/0902
+//but le programme lit un entier et qui détermine et affiche s’il s’agit d’un entier pair ou d’un entier impair
+//auteur Gabriel Gendron
+//date 2021/09/02
+
+#include iostream
+void main{
+ 
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
